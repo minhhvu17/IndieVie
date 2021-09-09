1fe4260bb691c57ed13f860a617a4ce9177a0a71
unknown <you@example.com>
2021-09-09 21:25:24 +0700

    1

---
+++ data.js
@@ -0,0 +1,541 @@
+const DATA = [
+    {
+        ids: 0,
+        artist: 'Ngọt',
+        artistImage: 'pj-img/artists/ngot.jpg',
+        description: 'Ngọt là nghệ sĩ trẻ 9X hoạt động âm nhạc độc lập (indie) nhiều năm qua giữa Hà Nội với 4 thành viên: Vũ Đinh Trọng Thắng, Nguyễn Hùng Nam Anh, Trần Bình Tuấn và Phan Việt Hoàng. Họ là một ban nhạc “đơn giản” với một trống, một guitar, một bass và một vocal (ca sĩ). Tháng 9/2015, sau gần 2 năm hoạt động, Ngọt quyết định thu âm album đầu tay. Quá trình thực hiện album đầu tay của Ngọt diễn ra trong 5 tháng. Tháng 2/2016, Ngọt chính thức hoàn thiện khâu thu âm với sự trợ giúp của những người bạn, cũng là nhà sản xuất tới từ Mỹ. Phần hậu kì của album đầu tay kéo dài trong 2 tháng với sự giúp đỡ từ các kĩ sư âm thanh ngoại quốc. Tháng 4/2016, Ngọt công bố ca khúc chính thức đầu tiên từ album, mang tên "Cho tôi đi theo".',
+        song: [
+            {
+                songName: 'Cho tôi lang thang',
+                songImage: 'pj-img/songIMG/Ngot/cho_toi_lang_thang.jpg',
+
+                audio: 'audio/Ngot/cho_toi_lang_thang.mp3',
+            },
+            {
+                songName: 'Em dạo này',
+                songImage: 'pj-img/songIMG/Ngot/em_dao_nay.jpg',
+
+                audio: 'audio/Ngot/em_dao_nay.mp3',
+            },
+            {
+                songName: 'Xanh',
+                songImage: 'pj-img/songIMG/Ngot/xanh.jpg',
+
+                audio: 'audio/Ngot/xanh.mp3',
+            },
+            {
+                songName: 'Lần cuối',
+                songImage: 'pj-img/songIMG/Ngot/lan_cuoi.jpg',
+
+                audio: 'audio/Ngot/lan_cuoi.mp3',
+            },
+            {
+                songName: 'Chuyển kênh',
+                songImage: 'pj-img/songIMG/Ngot/chuyen_kenh.jpg',
+
+                audio: 'audio/Ngot/chuyen_kenh.mp3',
+            },
+            {
+                songName: 'Hết thời',
+                songImage: 'pj-img/songIMG/Ngot/het_thoi.jpg',
+
+                audio: 'audio/Ngot/het_thoi.mp3',
+            },
+            {
+                songName: 'Tìm người nhà',
+                songImage: 'pj-img/songIMG/Ngot/tim_nguoi_nha.jpg',
+
+                audio: 'audio/Ngot/tim_nguoi_nha.mp3',
+            },
+            {
+                songName: 'Cho tôi đi theo',
+                songImage: 'pj-img/songIMG/Ngot/cho_toi_di_theo.jpg',
+
+                audio: 'audio/Ngot/cho_toi_di_theo.mp3',
+            },
+        ]
+
+    },
+    {
+        ids: 1,
+        artist: 'Vũ.',
+        artistImage: 'pj-img/artists/vu.jpg',
+        description: 'Vũ, được viết cách điệu là Vũ. (tên đầy đủ là Hoàng Thái Vũ, sinh ngày 3 tháng 10 năm 1995 tại Hà Nội) là ca sĩ kiêm sáng tác nhạc người Việt Nam. Sinh ra trong gia đình có bố là quân nhân và mẹ là giáo viên, Vũ thường đăng tải các sáng tác của mình trên Soundcloud. Thể loại của anh theo đuổi là nhạc indie pop, acoustic, rock.Trước khi đi hát, Vũ từng gia nhập quân ngũ và làm giảng viên tiếng Anh tại một trường quân sự. Đây cũng chính là khoảng thời gian anh sáng tác.',
+        song: [
+            {
+                songName: 'Lạ lùng',
+                songImage: 'pj-img/songIMG/Vu/la_lung.jpg',
+
+                audio: 'audio/Vu/la_lung.mp3',
+            },
+            {
+                songName: 'Đông kiếm em',
+                songImage: 'pj-img/songIMG/Vu/dong_kiem_em.jpg',
+
+                audio: 'audio/Vu/dong_kiem_em.mp3',
+            },
+            {
+                songName: 'Mùa mưa ngâu nằm cạnh',
+                songImage: 'pj-img/songIMG/Vu/mua_mua_ngau_nam_canh.jpg',
+
+                audio: 'audio/Vu/mua_mua_ngau_nam_canh.mp3',
+            },
+            {
+                songName: 'Lời yêu em',
+                songImage: 'pj-img/songIMG/Vu/loi_yeu_em.jpg',
+
+                audio: 'audio/Vu/loi_yeu_em.mp3',
+            },
+            {
+                songName: 'Hành tinh song song',
+                songImage: 'pj-img/songIMG/Vu/hanh_tinh_song_song.jpg',
+
+                audio: 'audio/Vu/hanh_tinh_song_song.mp3',
+            },
+            {
+                songName: 'Đợi',
+                songImage: 'pj-img/songIMG/Vu/doi.png',
+
+                audio: 'audio/Vu/doi.mp3',
+            },
+        ]
+
+    },
+    {
+        ids: 2,
+        artist: 'Nger',
+        artistImage: 'pj-img/artists/nger.jpg',
+        description: '',
+        song: [
+            {
+                songName: 'Anh đã quen ở bên cạnh em',
+                songImage: 'pj-img/songIMG/Nger/anh_da_quen_o_ben_canh_em.jpg',
+
+                audio: 'audio/Nger/anh_da_quen_o_ben_canh_em.mp3',
+            },
+            {
+                songName: 'Nói',
+                songImage: 'pj-img/songIMG/Nger/noi.jpg',
+
+                audio: 'audio/Nger/noi.mp3',
+            },
+            {
+                songName: 'Yêu em qua dòng tin nhắn',
+                songImage: 'pj-img/songIMG/Nger/yeu_em_qua_dong_tin_nhan.jpg',
+
+                audio: 'audio/Nger/yeu_em_qua_dong_tin_nhan.mp3',
+            },
+            {
+                songName: 'Muốn em là của riêng mình',
+                songImage: 'pj-img/songIMG/Nger/muon_em_la_cua_rieng_minh.jpg',
+
+                audio: 'audio/Nger/muon_em_la_cua_rieng_minh.mp3',
+            },
+            {
+                songName: 'Vương',
+                songImage: 'pj-img/songIMG/Nger/vuong.jpg',
+
+                audio: 'audio/Nger/vuong.mp3',
+            },
+            {
+                songName: 'Anh biết',
+                songImage: 'pj-img/songIMG/Nger/anh_biet.jpg',
+
+                audio: 'audio/Nger/anh_biet.mp3',
+            },
+            {
+                songName: 'Tình đắng như ly cà phê',
+                songImage: 'pj-img/songIMG/Nger/tinh_dang_nhu_ly_ca_phe.jpg',
+
+                audio: 'audio/Nger/tinh_dang_nhu_ly_ca_phe.mp3',
+            },
+        ]
+    },
+
+    {
+        ids: 3,
+        artist: 'Thịnh Suy',
+        artistImage: 'pj-img/artists/thinhsuy.jpg',
+        description: '',
+        song: [
+            {
+                songName: 'Một đêm say',
+                songImage: 'pj-img/songIMG/Thinh_Suy/mot_dem_say.jpg',
+
+                audio: 'audio/Thinh_Suy/mot_dem_say.mp3',
+            },
+            {
+                songName: 'Chuyện rằng',
+                songImage: 'pj-img/songIMG/Thinh_Suy/chuyen_rang.jpg',
+
+                audio: 'audio/Thinh_Suy/chuyen_rang.mp3',
+            },
+            {
+                songName: 'Thắc mắc',
+                songImage: 'pj-img/songIMG/Thinh_Suy/thac_mac.jpg',
+
+                audio: 'audio/Thinh_Suy/thac_mac.mp3',
+            },
+            {
+                songName: 'Mai mình xa',
+                songImage: 'pj-img/songIMG/Thinh_Suy/mai_minh_xa.jpg',
+
+                audio: 'audio/Thinh_Suy/mai_minh_xa.mp3',
+            },
+            {
+                songName: 'Nghe em',
+                songImage: 'pj-img/songIMG/Thinh_Suy/nghe_em.jpg',
+
+                audio: 'audio/Thinh_Suy/nghe_em.mp3',
+            },
+        ]
+    },
+    {
+        ids: 4,
+        artist: 'Hoàng Dũng',
+        artistImage: 'pj-img/artists/hoangdung.jpg',
+        description: 'Nguyễn Hoàng Dũng, hay còn được biết đến với nghệ danh Hoàng Dũng (sinh ngày 04/11/1995) là một nam ca sĩ, nhạc sĩ tại Việt Nam, anh từng đạt danh hiệu Á Quân cuộc thi Giọng Hát Việt năm 2015 và lọt top 10 Bài hát hay nhất năm 2016. Với lối tư duy văn minh trong âm nhạc, khả năng sáng tác và giọng hát nội lực, Hoàng Dũng được đánh giá là một nghệ sỹ tiềm năng và đang dần khẳng định phong cách cũng như vị trí của mình trong lớp nghệ sỹ trẻ tại Việt Nam.',
+        song: [
+            {
+                songName: 'Yếu đuối',
+                songImage: 'pj-img/songIMG/Hoang_Dung/yeu_duoi.jpg',
+                releaseYear: '2015',
+                audio: 'audio/Hoang_Dung/yeu_duoi.mp3',
+            },
+            {
+                songName: 'Tôi là ai trong em',
+                songImage: 'pj-img/songIMG/Hoang_Dung/toi_la_ai_trong_em.jpg',
+                releaseYear: '2016',
+                audio: 'audio/Hoang_Dung/toi_la_ai_trong_em.mp3',
+            },
+            {
+                songName: 'Đi đâu để thấy hoa bay',
+                songImage: 'pj-img/songIMG/Hoang_Dung/di_dau_de_thay_hoa_bay.jpg',
+                releaseYear: '2017',
+                audio: 'audio/Hoang_Dung/di_dau_de_thay_hoa_bay.mp3',
+            },
+            {
+                songName: 'Chẳng nói nên lời',
+                songImage: 'pj-img/songIMG/Hoang_Dung/chang_noi_nen_loi.jpg',
+                releaseYear: '2017',
+                audio: 'audio/Hoang_Dung/chang_noi_nen_loi.mp3',
+            },
+            {
+                songName: 'Đôi lời',
+                songImage: 'pj-img/songIMG/Hoang_Dung/doi_loi.jpg',
+                releaseYear: '2018',
+                audio: 'audio/Hoang_Dunng/doi_loi.mp3',
+            },
+            {
+                songName: 'Nép vào anh và nghe anh hát',
+                songImage: 'pj-img/songIMG/Hoang_Dung/nep_vao_anh_va_nghe_anh_hat.jpg',
+                releaseYear: '2018',
+                audio: 'audio/Hoang_Dung/nep_vao_anh_va_nghe_anh_hat.mp3',
+            },
+            {
+                songName: 'Đôi lời tình ca',
+                songImage: 'pj-img/songIMG/Hoang_Dung/doi_loi_tinh_ca.jpg',
+                releaseYear: '2019',
+                audio: 'audio/Hoang_Dung/doi_loi_tinh_ca.mp3',
+            },
+            {
+                songName: 'Nàng thơ',
+                songImage: 'pj-img/songIMG/Hoang_Dung/nang_tho.jpg',
+                releaseYear: '2020',
+                audio: 'audio/Hoang_Dung/nang_tho.mp3',
+            },
+        ]
+
+    },
+    {
+        ids: 5,
+        artist: 'Hải Sâm',
+        artistImage: 'pj-img/artists/haisam.jpg',
+        description: '',
+        song: [
+            {
+                songName: 'Thế thôi',
+                songImage: 'pj-img/songIMG/Hai_Sam/the_thoi.jpg',
+
+                audio: 'audio/Hai_Sam/the_thoi.mp3',
+            },
+            {
+                songName: 'Một điều mà anh rất ngại nói ra',
+                songImage: 'pj-img/songIMG/Hai_Sam/mot_dieu_ma_anh_rat_ngai_noi_ra.jpg',
+
+                audio: 'audio/Hai_Sam/mot_dieu_ma_anh_rat_ngai_noi_ra.mp3',
+            },
+            {
+                songName: 'HENNESSY',
+                songImage: 'pj-img/songIMG/Hai_Sam/hennessy.jpg',
+
+                audio: 'audio/Hai_Sam/hennessy.mp3',
+            },
+            {
+                songName: 'Em có đợi tin nhắn tôi không',
+                songImage: 'pj-img/songIMG/Hai_Sam/em_co_doi_tin_nhan_toi_khong.jpg',
+
+                audio: 'audio/Hai_Sam/em_co_doi_tin_nhan_toi_khong.mp3',
+            },
+            {
+                songName: 'Cuối chiều',
+                songImage: 'pj-img/songIMG/Hai_Sam/cuoi_chieu.jpg',
+
+                audio: 'audio/Hai_Sam/cuoi_chieu.mp3',
+            },
+        ]
+
+    },
+    {
+        ids: 6,
+        artist: 'Cá Hồi Hoang',
+        artistImage: 'pj-img/artists/cahoihoang.jpg',
+        description: '',
+        song: [
+            {
+                songName: 'Tầng thượng 102',
+                songImage: 'pj-img/songIMG/Ca_Hoi_Hoang/tang_thuong_102.jpg',
+
+                audio: 'audio/Ca_Hoi_Hoang/tang_thuong_102.mp3',
+            },
+            {
+                songName: 'Beertalks',
+                songImage: 'pj-img/songIMG/Ca_Hoi_Hoang/beertalks.jpg',
+
+                audio: 'audio/Ca_Hoi_Hoang/beertalks.mp3',
+            },
+            {
+                songName: '5AM',
+                songImage: 'pj-img/songIMG/Ca_Hoi_Hoang/5am.jfif',
+
+                audio: 'audio/Ca_Hoi_Hoang/5am.mp3',
+            },
+            {
+                songName: 'Điền vào ô trống',
+                songImage: 'pj-img/songIMG/Ca_Hoi_Hoang/dien_vao_o_trong.jpg',
+
+                audio: 'audio/Ca_Hoi_Hoang/dien_vao_o_trong.mp3',
+            },
+            {
+                songName: 'Hiệu ứng trốn chạy',
+                songImage: 'pj-img/songIMG/Ca_Hoi_Hoang/hieu_ung_tron_chay.jfif',
+
+                audio: 'audio/Ca_Hoi_Hoang/hieu_ung_tron_chay.mp3',
+            },
+        ]
+
+    },
+    {
+        ids: 7,
+        artist: 'Pink Frog',
+        artistImage: 'pj-img/artists/pinkfrog.jpg',
+        description: 'Pink Frog, tên thật là Vũ Thành Lộc. Pink Frog bắt đầu chơi nhạc từ năm 2009. Bài hát đầu – Break your man được viết năm 2013. Sau một khoảng thời gian ngừng sáng tác, đến đầu năm 2017 anh bắt đầu viết trở lại, với ͞Tôi và em. Nhạc Pink Frog viết hầu hết là những suy nghĩ và tình cảm dưới góc nhìn cá nhân về tình yêu và những con người xung quanh cuộc sống thường nhật của anh. Lộc luôn mong muốn có thể đem một chút âm hưởng dân gian để hát lên những câu chuyện của mình.',
+        song: [
+            {
+                songName: 'Tôi và em',
+                songImage: 'pj-img/songIMG/Pink_Frog/toi_va_em.jpg',
+
+                audio: 'audio/Pink_Frog/toi_va_em.mp3',
+            },
+            {
+                songName: 'Ô cửa',
+                songImage: 'pj-img/songIMG/Pink_Frog/o_cua.jpg',
+
+                audio: 'audio/Pink_Frog/o_cua.mp3',
+            },
+            {
+                songName: 'Câu chuyện',
+                songImage: 'pj-img/songIMG/Pink_Frog/cau_chuyen.jpg',
+
+                audio: 'audio/Pink_Frog/cau_chuyen.mp3',
+            },
+            {
+                songName: 'Nghe',
+                songImage: 'pj-img/songIMG/Pink_Frog/nghe.jpg',
+
+                audio: 'audio/Pink_Frog/nghe.mp3',
+            },
+        ]
+
+    },
+    {
+        ids: 8,
+        artist: 'Kiên',
+        artistImage: 'pj-img/artists/kien.jpg',
+        description: 'Kiên là cậu trai trẻ Hà Thành với một chút phớt đời nhưng lại có tình yêu to lớn với âm nhạc. Đến với âm nhạc của Kiên, người nghe sẽ được lắng nghe những dòng suy nghĩ có khi rất chi đời thường',
+        song: [
+            {
+                songName: 'Con giời',
+                songImage: 'pj-img/songIMG/Kien/con_gioi.jpg',
+
+                audio: 'audio/Kien/con_gioi.mp3',
+            },
+            {
+                songName: 'Em ăn sáng chưa',
+                songImage: 'pj-img/songIMG/Kien/em_an_sang_chua.jpg',
+
+                audio: 'audio/Kien/em_an_sang_chua.mp3',
+            },
+            {
+                songName: 'Hai buổi cà phê',
+                songImage: 'pj-img/songIMG/Kien/hai_buoi_ca_phe.jpg',
+
+                audio: 'audio/Kien/hai_buoi_ca_phe.mp3',
+            },
+            {
+                songName: 'Quả tim màu lửa',
+                songImage: 'pj-img/songIMG/Kien/qua_tim_mau_lua.jpg',
+
+                audio: 'audio/Kien/qua_tim_mau_lua.mp3',
+            },
+            {
+                songName: 'Tại sao?',
+                songImage: 'pj-img/songIMG/Kien/tai_sao.jpg',
+
+                audio: 'audio/Kien/tai_sao.mp3',
+            },
+            {
+                songName: 'Tập thể dục',
+                songImage: 'pj-img/songIMG/Kien/tap_the_duc.jpg',
+
+                audio: 'audio/Kien/tap_the_duc.mp3',
+            },
+        ]
+
+    },
+    {
+        ids: 9,
+        artist: 'Chillies',
+        artistImage: 'pj-img/artists/chillies.jpg',
+        description: '',
+        song : [
+            {
+                songName: 'Có em đời bỗng vui',
+                songImage: 'pj-img/songIMG/Chillies/co_em_doi_bong_vui.jpg',
+
+                audio: 'audio/Chillies/co_em_doi_bong_vui.mp3',
+            },
+            {
+                songName: 'Cứ chill thôi',
+                songImage: 'pj-img/songIMG/Chillies/cu_chill_thoi.jpg',
+
+                audio: 'audio/Chillies/cu_chill_thoi.mp3',
+            },
+            {
+                songName: 'Mascara',
+                songImage: 'pj-img/songIMG/Chillies/mascara.jpg',
+
+                audio: 'audio/Chillies/mascara.mp3',
+            },
+            {
+                songName: 'Nếu ngày mai không đến',
+                songImage: 'pj-img/songIMG/Chillies/neu_ngay_mai_khong_den.jpg',
+
+                audio: 'audio/Chillies/neu_ngay_mai_khong_den.mp3',
+            },
+            {
+                songName: 'Những con đường song song',
+                songImage: 'pj-img/songIMG/Chillies/nhung_con_duong_song_song.jpg',
+
+                audio: 'audio/Chillies/nhung_con_duong_song_song.mp3',
+            },
+            {
+                songName: 'Và thế là hết',
+                songImage: 'pj-img/songIMG/Chillies/va_the_la_het.jpg',
+
+                audio: 'audio/Chillies/va_the_la_het.mp3',
+            },
+            {
+                songName: 'Vùng ký ức',
+                songImage: 'pj-img/songIMG/Chillies/vung_ky_uc.jpg',
+
+                audio: 'audio/Chillies/vung_ky_uc.mp3',
+            },
+            {
+                songName: 'Who?',
+                songImage: 'pj-img/songIMG/Chillies/who.jpg',
+
+                audio: 'audio/Chillies/who.mp3',
+            },
+        ]
+
+    },
+    // {
+    //     ids: 10,
+    //     artist: '',
+    //     artistImage: 'pj-img/artists/.jpg',
+    //     description: '',
+    //     song : [
+    //         {
+    //             songName: '',
+    //             songImage: '',
+
+    //             audio: '',
+    //         },
+    //         {
+    //             songName: '',
+    //             songImage: '',
+
+    //             audio: '',
+    //         },
+    //         {
+    //             songName: '',
+    //             songImage: '',
+
+    //             audio: '',
+    //         },
+    //         {
+    //             songName: '',
+    //             songImage: '',
+
+    //             audio: '',
+    //         },
+    //         {
+    //             songName: '',
+    //             songImage: '',
+
+    //             audio: '',
+    //         },
+    //     ]
+
+    // },
+    // {
+    //     ids: 11,
+    //     artist: '',
+    //     artistImage: 'pj-img/artists/.jpg',
+    //     description: '',
+    //     song : [
+    //         {
+    //             songName: '',
+    //             songImage: '',
+
+    //             audio: '',
+    //         },
+    //         {
+    //             songName: '',
+    //             songImage: '',
+
+    //             audio: '',
+    //         },
+    //         {
+    //             songName: '',
+    //             songImage: '',
+
+    //             audio: '',
+    //         },
+    //         {
+    //             songName: '',
+    //             songImage: '',
+
+    //             audio: '',
+    //         },
+    //         {
+    //             songName: '',
+    //             songImage: '',
+
+    //             audio: '',
+    //         },
+    //     ]
+
+    // },
+]
---
+++ script.js
@@ -0,0 +1,104 @@
+let content = document.getElementById('each-slider');
+let content2 = document.getElementById('each-slider2');
+let content3 = document.getElementById('artistList');
+
+// De xuat cho ban 
+for (let i = 0; i < 5; i++) {
+    let randomArtist = Math.floor(Math.random() * DATA.length);
+    // console.log(randomArtist);
+    let random = Math.floor(Math.random() * DATA[randomArtist].song.length);
+
+    var ngheSi = DATA[randomArtist].artist;
+    var baiHat = DATA[randomArtist].song[random].songName;
+    var anhBaiHat = DATA[randomArtist].song[random].songImage;
+
+    var characterHTML = `
+    <div class="each-slider-title">
+        <div class="each-slider-img" style="background-image: url('${anhBaiHat}')">
+            <div class="playable">
+                <svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 16 31" class="play-music">
+                    <path
+                        d="M15.552 15.168q0.448 0.32 0.448 0.832 0 0.448-0.448 0.768l-13.696 8.512q-0.768 0.512-1.312 0.192t-0.544-1.28v-16.448q0-0.96 0.544-1.28t1.312 0.192z">
+                    </path>
+                </svg>
+            </div>
+            <div class="addFavor">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 4.248c-3.148-5.402-12-3.825-12 2.944 0 4.661 5.571 9.427 12 15.808 6.43-6.381 12-11.147 12-15.808 0-6.792-8.875-8.306-12-2.944z" class="changeHeartColor"</path></svg>
+            </div>
+        </div>
+        <div class="song-title">
+            <div class="nameSong" style="font-size:15px">${baiHat}</div>
+            <div>${ngheSi}</div>
+        </div>
+    </div>
+    `;
+    content.insertAdjacentHTML('beforeend', characterHTML);
+}
+
+
+// Hit cua tuan
+let hitList = [];
+let hitArtist = [];
+hitList.push(DATA[4].song[7]);
+hitList.push(DATA[3].song[4]);
+hitList.push(DATA[4].song[6]);
+hitList.push(DATA[1].song[0]);
+hitList.push(DATA[5].song[2]);
+
+hitArtist.push(DATA[4].artist);
+hitArtist.push(DATA[3].artist);
+hitArtist.push(DATA[4].artist);
+hitArtist.push(DATA[1].artist);
+hitArtist.push(DATA[5].artist);
+
+for (let i = 0; i < 5; i++) {
+    var characterHTML = `
+    <div class="each-slider-title">
+        <div class="each-slider-img" style="background-image: url('${hitList[i].songImage}')">
+            <div class="playable">
+                <svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 16 31" class="play-music">
+                    <path
+                        d="M15.552 15.168q0.448 0.32 0.448 0.832 0 0.448-0.448 0.768l-13.696 8.512q-0.768 0.512-1.312 0.192t-0.544-1.28v-16.448q0-0.96 0.544-1.28t1.312 0.192z">
+                    </path>
+                </svg>
+                
+            </div>
+            <div class="addFavor">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 4.248c-3.148-5.402-12-3.825-12 2.944 0 4.661 5.571 9.427 12 15.808 6.43-6.381 12-11.147 12-15.808 0-6.792-8.875-8.306-12-2.944z" class="changeHeartColor"</path></svg>
+            </div>
+        </div>
+        <div class="song-title">
+            <div style="font-size:15px" class="nameSong">${hitList[i].songName}</div>
+            <div>${hitArtist[i]}</div>
+        </div>
+    </div>
+    `;
+    content2.insertAdjacentHTML('beforeend', characterHTML);
+}
+
+
+
+
+// Ca sĩ nổi bật
+for (let i = 0; i < 4; i++) {
+    var artistOdd = DATA[i].artist;
+    var trackNumber = DATA[i].song.length;
+    var artistImg = DATA[i].artistImage;
+    var characterHTML = `
+    <li>
+        <div><img class="artistImg" src="${artistImg}" alt="">
+        
+        </div>
+        <div class="artistContent">
+            <div style="cursor:pointer">${artistOdd}</div>
+            <div class="follow">
+                <div class="trackNum">${trackNumber}<span><img src="pj-img/record.png" alt=""></span></div>
+                <div id="followBtn"><button>Theo dõi <span><img src="pj-img/follow.png" alt=""></span></button></div>
+            </div>
+        </div>
+    </li>
+    `;
+    content3.insertAdjacentHTML('beforeend', characterHTML);
+}
+
+
